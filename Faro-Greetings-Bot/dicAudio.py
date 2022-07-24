f88ffaf11f3a0d8940f699c504bf12cee0412bb6
GustaBeholder <50680368+GustaBeholder@users.noreply.github.com>
2022-07-24 02:42:00 -0300

    Initial Commit

---
+++ Faro-Greetings-Bot/dicAudio.py
@@ -0,0 +1,15 @@
+
+
+audios = {
+    1: 'cavalo-rodrigo-faro.mp3',
+    2: 'danca-gatinho.mp3',
+    3: 'tome.mp3',
+    4: 'ui-rodrigo-faro.mp3',
+    5: 'som-inentendivel-rodrigo-faro.mp3',
+    6: 'nao-rodrigo-faro.mp3',
+    7: 'ele-g0sta.mp3',
+    8: 'que-isso-meu-filho-calma-vai-dar-namoro.mp3',
+    9: 'demaals.mp3',
+    10: 'chega-rodrigo-faro.mp3'
+}
+
---
+++ Faro-Greetings-Bot/olha_ele_ai.py
@@ -0,0 +1,56 @@
+import discord
+import random
+from discord.ext import commands
+from dicAudio import audios
+
+token = 'Token Goes Here'
+pathFF = 'C:/Users/Aluno/Downloads/ffmpeg-2022-07-21-git-f7d510b33f-essentials_build/bin/ffmpeg.exe'
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+    print(f'Bot online {format(bot.user)}')
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await ctx.send(f'Entrando no canal {channel.name}')
+    await channel.connect()        
+
+@bot.command()
+async def leave(ctx):
+    await ctx.send(f'Saindo do canal {ctx.author.voice.channel.name}')
+    await ctx.voice_client.disconnect()   
+
+@bot.command()
+async def play(ctx):
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio(executable=pathFF, source=f"audios/{audios[random.randint(1,len(audios))]}")
+    if not voice_client.is_playing():
+        voice_client.play(audio_source, after=None)
+
+@bot.event
+async def on_voice_state_update(member: discord.Member, before, after):
+    guild = member.guild
+    vc_before = before.channel
+    vc_after = after.channel
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio(executable=pathFF, source="audios/olhaEleAi.mp3")
+    
+    if vc_before == vc_after:
+        return
+    if vc_before is None:
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=None)
+    elif vc_after is None:
+        return
+    else:
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=None)
+
+    
+bot.run(token)
+
+
